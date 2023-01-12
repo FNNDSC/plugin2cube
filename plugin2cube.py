046070de7f26619ddec679662c4f995777f4e7da
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-01-12 09:56:47 -0500

    Bump to 1.0.12 for resync

--- plugin2cube.py
+++ plugin2cube.py
@@ -29,7 +29,7 @@ from    control.filter          import PathFilter
 
 Env             = data.env()
 
-__version__ = '1.0.10'
+__version__ = '1.0.12'
 
 DISPLAY_TITLE = r"""
        _             _        _____            _
